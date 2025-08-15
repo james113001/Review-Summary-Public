098f230951990d1b5c844bd49d8a295763e69a3f
James Kim <44733004+james113001@users.noreply.github.com>
2025-08-16 00:56:52 +0100

    Update summarize_reviews.py

--- summarize_reviews.py
+++ summarize_reviews.py
@@ -9,8 +9,8 @@ load_dotenv()
 # ------------------------------------
 # CONFIGURATION
 # ------------------------------------
-DB_HOST = 'db'
-DB_PORT = 15432
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("POSTGRES_DB")
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
